40ee2a6063bd442cbe1301ffa94650cd574ada40
Adam Goldberg <goldbergusername@gmail.com>
2019-04-11 14:10:43 -0500

    The state as of on-site demo 4/10/19.  Cleanup and improvements remain.

--- constants.js
+++ constants.js
@@ -1,8 +1,8 @@
 // constants.js
 
 // Default datafile provided to get up and running quickly. Replace with your own!
-const datafileURL = 'https://cdn.optimizely.com/datafiles/CpqC7ch493FEqa3HzY3963.json';
+const datafileURL = 'https://cdn.optimizely.com/json/14285220003.json';
 
 module.exports = {
-  datafileURL
-}
+    datafileURL
+};
